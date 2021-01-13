2b14052be1eb618ef763243639f2f965b2132b36
Rich0721 <zx20125566@gmail.com>
2021-01-13 17:32:03 +0800

    Modify train to 300 and 512 version

--- models/ssd_512.py
+++ models/ssd_512.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import tensorflow as tf
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Reshape, ZeroPadding2D, Concatenate, Input, Activation, Flatten
 from tensorflow.keras.models import Model
@@ -168,4 +169,176 @@ def SSD512(img_size, n_classes, l2_regularization=5e-4,
     
     model = Model(inputs=x, outputs=predictions)
     model.summary()
+=======
+import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Reshape, ZeroPadding2D, Concatenate, Input, Activation, Flatten
+from tensorflow.keras.models import Model
+from tensorflow.keras.regularizers import l2
+
+
+from ssd_keras_layers.anchorBoxes import AnchorBoxes
+from ssd_keras_layers.normalize import Normalize
+
+def SSD512(img_size, n_classes, l2_regularization=5e-4,
+            anchors=[30, 60, 111, 162, 213, 264, 315],
+            variances=[0.1, 0.1, 0.2, 0.2]):
+    
+    classes = n_classes + 1# Account for the background class.
+    n_boxes = [4, 6, 6, 6, 6, 4, 4]
+    
+    # Build network
+
+    x = Input(shape=(img_size))
+    # 512
+    conv1_1 = Conv2D(64, (3, 3), activation='relu', padding='same', name='conv1_1')(x)
+    conv1_2 = Conv2D(64, (3, 3), activation='relu', padding='same', name='conv1_2')(conv1_1)
+    pool1 = MaxPooling2D((2, 2), strides=(2, 2), padding='same', name='pool1')(conv1_2)
+
+    # 256
+    conv2_1 = Conv2D(128, (3, 3), activation='relu', padding='same', name='conv2_1')(pool1)
+    conv2_2 = Conv2D(128, (3, 3), activation='relu', padding='same', name='conv2_2')(conv2_1)
+    pool2 = MaxPooling2D((2, 2), strides=(2, 2), padding='same', name='pool2')(conv2_2)
+
+    # 128
+    conv3_1 = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv3_1')(pool2)
+    conv3_2 = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv3_2')(conv3_1)
+    conv3_3 = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv3_3')(conv3_2)
+    pool3 = MaxPooling2D((2, 2), strides=(2, 2), padding='same', name='pool3')(conv3_3)
+
+    # 64
+    conv4_1 = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv4_1')(pool3)
+    conv4_2 = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv4_2')(conv4_1)
+    conv4_3 = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv4_3')(conv4_2)
+    pool4 = MaxPooling2D((2, 2), strides=(2, 2), padding='same', name='pool4')(conv4_3)
+
+    # 32
+    conv5_1 = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv5_1')(pool4)
+    conv5_2 = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv5_2')(conv5_1)
+    conv5_3 = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv5_5')(conv5_2)
+    pool5 = MaxPooling2D((2, 2), strides=(1, 1), padding='same', name='pool5')(conv5_3)
+
+    print("Shape:{}".format(pool5.shape))
+    fc6 = Conv2D(1024, (3, 3), activation='relu', dilation_rate=(6, 6), padding='same', name='fc6')(pool5)
+    fc7 = Conv2D(1024, (1, 1), activation='relu', padding='same', name='fc7')(fc6)
+    
+    # 16
+    conv6_1 = Conv2D(256, (1, 1), activation='relu', padding='same', name='conv6_1')(fc7)
+    conv6_1 = ZeroPadding2D(padding=((1, 1), (1, 1)), name='conv6_padding')(conv6_1)
+    conv6_2 = Conv2D(512, (3, 3), strides=(2, 2), activation='relu', padding='valid', name='conv6_2')(conv6_1)
+    
+    # 8
+    conv7_1 = Conv2D(128, (1, 1), activation='relu', padding='same', name='conv7_1')(conv6_2)
+    conv7_1 = ZeroPadding2D(padding=((1, 1), (1, 1)), name='conv7_padding')(conv7_1)
+    conv7_2 = Conv2D(256, (3, 3), strides=(2, 2), activation='relu', padding='valid', name='conv7_2')(conv7_1)
+    
+    # 6
+    conv8_1 = Conv2D(128, (1, 1), activation='relu', padding='same', name='conv8_1')(conv7_2)
+    conv8_1 = ZeroPadding2D(padding=((1, 1), (1, 1)), name='conv8_padding')(conv8_1)
+    conv8_2 = Conv2D(256, (3, 3), strides=(2, 2), activation='relu', padding='valid', name='conv8_2')(conv8_1)
+    
+    # 4
+    conv9_1 = Conv2D(128, (1, 1), activation='relu', padding='same',  name='conv9_1')(conv8_2)
+    conv9_1 = ZeroPadding2D(padding=((1, 1), (1, 1)), name='conv9_padding')(conv9_1)
+    conv9_2 = Conv2D(256, (3, 3), strides=(2, 2), activation='relu', padding='valid', name='conv9_2')(conv9_1)
+
+    # 2
+    conv10_1 = Conv2D(128, (1, 1), activation='relu', padding='same',  name='conv10_1')(conv9_2)
+    conv10_1 = ZeroPadding2D(padding=((1, 1), (1, 1)), name='conv10_padding')(conv10_1)
+    conv10_2 = Conv2D(256, (3, 3), strides=(2, 2), activation='relu', padding='valid', name='conv10_2')(conv10_1)
+    
+    # Build SSD network
+
+    conv4_3_norm = Normalize(20, name='conv4_3_norm')(conv4_3)
+
+    # Build "n_classes" confidence values for each box. Ouput shape: (b, h, w, n_boxes*n_classes)
+    conv4_3_norm_mbox_conf = Conv2D(n_boxes[0] * classes, (3, 3), padding='same',  name='conv4_3_norm_mbox_conf')(conv4_3_norm)
+    fc7_mbox_conf = Conv2D(n_boxes[1] * classes, (3, 3), padding='same', name='fc7_mbox_conf')(fc7)
+    conv6_2_mbox_conf = Conv2D(n_boxes[2] * classes, (3, 3), padding='same', name='conv6_2_mbox_conf')(conv6_2)
+    conv7_2_mbox_conf = Conv2D(n_boxes[3] * classes, (3, 3), padding='same',  name='conv7_2_mbox_conf')(conv7_2)
+    conv8_2_mbox_conf = Conv2D(n_boxes[4] * classes, (3, 3), padding='same', name='conv8_2_mbox_conf')(conv8_2)
+    conv9_2_mbox_conf = Conv2D(n_boxes[5] * classes, (3, 3), padding='same', name='conv9_2_mbox_conf')(conv9_2)
+    conv10_2_mbox_conf = Conv2D(n_boxes[6] * classes, (3, 3), padding='same', name='conv10_2_mbox_conf')(conv10_2)
+
+    # Build 4 box coordinates for each box. Output shape: (b, h, w, n_boxes * 4)
+    conv4_3_norm_mbox_loc = Conv2D(n_boxes[0] * 4, (3, 3), padding='same', name='conv4_3_norm_mbox_loc')(conv4_3_norm)
+    fc7_mbox_loc = Conv2D(n_boxes[1] * 4, (3, 3), padding='same', name='fc7_mbox_loc')(fc7)
+    conv6_2_mbox_loc = Conv2D(n_boxes[2] * 4, (3, 3), padding='same', name='conv6_2_mbox_loc')(conv6_2)
+    conv7_2_mbox_loc = Conv2D(n_boxes[3] * 4, (3, 3), padding='same',  name='conv7_2_mbox_loc')(conv7_2)
+    conv8_2_mbox_loc = Conv2D(n_boxes[4] * 4, (3, 3), padding='same', name='conv8_2_mbox_loc')(conv8_2)
+    conv9_2_mbox_loc = Conv2D(n_boxes[5] * 4, (3, 3), padding='same', name='conv9_2_mbox_loc')(conv9_2)
+    conv10_2_mbox_loc = Conv2D(n_boxes[6] * 4, (3, 3), padding='same', name='conv10_2_mbox_loc')(conv10_2)
+    
+    # Generate the anchor boxes. Output shape: (b, h, w, n_boxes, 8)
+    conv4_3_norm_mbox_priorbox = AnchorBoxes(img_size=img_size, min_size=anchors[0], max_size=anchors[1],aspect_ratios=[2],
+                                             variances=variances,name='conv4_3_norm_mbox_priorbox')(conv4_3_norm)
+    fc7_mbox_priorbox = AnchorBoxes(img_size=img_size, min_size=anchors[1], max_size=anchors[2],aspect_ratios=[2, 3],
+                                             variances=variances,name='fc7_mbox_priorbox')(fc7)
+    conv6_2_mbox_priorbox = AnchorBoxes(img_size=img_size, min_size=anchors[2], max_size=anchors[3],aspect_ratios=[2, 3],
+                                             variances=variances, name='conv6_2_mbox_priorbox')(conv6_2)
+    conv7_2_mbox_priorbox = AnchorBoxes(img_size=img_size, min_size=anchors[3], max_size=anchors[4],aspect_ratios=[2, 3],
+                                             variances=variances, name='conv7_2_mbox_priorbox')(conv7_2)
+    conv8_2_mbox_priorbox = AnchorBoxes(img_size=img_size, min_size=anchors[4], max_size=anchors[5],aspect_ratios=[2, 3],
+                                             variances=variances, name='conv8_2_mbox_priorbox')(conv8_2)
+    conv9_2_mbox_priorbox = AnchorBoxes(img_size=img_size, min_size=anchors[5], max_size=anchors[6],aspect_ratios=[2],
+                                        variances=variances, name='conv9_2_mbox_priorbox')(conv9_2)
+    conv10_2_mbox_priorbox = AnchorBoxes(img_size=img_size, min_size=anchors[6], max_size=anchors[7],aspect_ratios=[2],
+                                        variances=variances, name='conv10_2_mbox_priorbox')(conv10_2)
+
+    ### Reshape
+
+    # Reshape the class predictions, yielding 3D tensors of shape `(batch, height * width * n_boxes, n_classes)`
+    # We want the classes isolated in the last axis to perform softmax on them
+    conv4_3_norm_mbox_conf_reshape = Flatten(name='conv4_3_norm_mbox_conf_reshape')(conv4_3_norm_mbox_conf)
+    fc7_mbox_conf_reshape = Flatten(name='fc7_mbox_conf_reshape')(fc7_mbox_conf)
+    conv6_2_mbox_conf_reshape = Flatten(name='conv6_2_mbox_conf_reshape')(conv6_2_mbox_conf)
+    conv7_2_mbox_conf_reshape = Flatten(name='conv7_2_mbox_conf_reshape')(conv7_2_mbox_conf)
+    conv8_2_mbox_conf_reshape = Flatten(name='conv8_2_mbox_conf_reshape')(conv8_2_mbox_conf)
+    conv9_2_mbox_conf_reshape = Flatten(name='conv9_2_mbox_conf_reshape')(conv9_2_mbox_conf)
+    conv10_2_mbox_conf_reshape = Flatten(name='conv10_2_mbox_conf_reshape')(conv10_2_mbox_conf)
+    # Reshape the box predictions, yielding 3D tensors of shape `(batch, height * width * n_boxes, 4)`
+    # We want the four box coordinates isolated in the last axis to compute the smooth L1 loss
+    conv4_3_norm_mbox_loc_reshape = Flatten(name='conv4_3_norm_mbox_loc_reshape')(conv4_3_norm_mbox_loc)
+    fc7_mbox_loc_reshape = Flatten(name='fc7_mbox_loc_reshape')(fc7_mbox_loc)
+    conv6_2_mbox_loc_reshape = Flatten(name='conv6_2_mbox_loc_reshape')(conv6_2_mbox_loc)
+    conv7_2_mbox_loc_reshape = Flatten(name='conv7_2_mbox_loc_reshape')(conv7_2_mbox_loc)
+    conv8_2_mbox_loc_reshape = Flatten(name='conv8_2_mbox_loc_reshape')(conv8_2_mbox_loc)
+    conv9_2_mbox_loc_reshape = Flatten(name='conv9_2_mbox_loc_reshape')(conv9_2_mbox_loc)
+    conv10_2_mbox_loc_reshape = Flatten(name='conv10_2_mbox_loc_reshape')(conv10_2_mbox_loc)
+
+    ### Concatenate the predictions from the different layers
+
+    mbox_conf = Concatenate(axis=1, name='mbox_conf')([conv4_3_norm_mbox_conf_reshape,
+                                                       fc7_mbox_conf_reshape,
+                                                       conv6_2_mbox_conf_reshape,
+                                                       conv7_2_mbox_conf_reshape,
+                                                       conv8_2_mbox_conf_reshape,
+                                                       conv9_2_mbox_conf_reshape,
+                                                       conv10_2_mbox_conf_reshape])
+    mbox_loc = Concatenate(axis=1, name='mbox_loc')([conv4_3_norm_mbox_loc_reshape,
+                                                     fc7_mbox_loc_reshape,
+                                                     conv6_2_mbox_loc_reshape,
+                                                     conv7_2_mbox_loc_reshape,
+                                                     conv8_2_mbox_loc_reshape,
+                                                     conv9_2_mbox_loc_reshape,
+                                                     conv10_2_mbox_loc_reshape])
+    mbox_priorbox = Concatenate(axis=1, name='mbox_priorbox')([conv4_3_norm_mbox_priorbox,
+                                                               fc7_mbox_priorbox,
+                                                               conv6_2_mbox_priorbox,
+                                                               conv7_2_mbox_priorbox,
+                                                               conv8_2_mbox_priorbox,
+                                                               conv9_2_mbox_priorbox,
+                                                               conv10_2_mbox_priorbox])
+    
+    mbox_loc = Reshape((-1, 4), name='mbox_loc_final')(mbox_loc)
+    mbox_conf = Reshape((-1, classes), name='mbox_conf_logits')(mbox_conf)
+    mbox_conf = Activation('softmax', name='mbox_conf_final')(mbox_conf)
+
+    predictions = Concatenate(axis=2, name='predictions')([mbox_loc,
+                            mbox_conf,
+                            mbox_priorbox])
+    
+    
+    model = Model(inputs=x, outputs=predictions)
+    model.summary()
+>>>>>>> Stashed changes
     return model
